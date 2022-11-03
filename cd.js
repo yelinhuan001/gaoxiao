3f12d654436533f21c692b4936bbed4e532e40da
yelinhuan001 <61222411+yelinhuan001@users.noreply.github.com>
2022-11-03 12:00:05 +0800

    Update cd.js

--- cd.js
+++ cd.js
@@ -8,14 +8,13 @@
 群1077223830
 *******************************
 [rewrite_local]
-^http[s]?:\/\/api.yonekura.cn\/3.6.8\/uicommon\/getuser.+$ url script-response-body https://raw.githubusercontent.com/yelinhuan001/gaoxiao/main/cd.js
-[mitm] 
+^http[s]?:\/\/api.yonekura.cn\/3.6.8\/uicommon\/getuser.+$ url script-response-body yonekura.js
 hostname = *.yonekura.*
 *******************************
 Surge
 
 [Script]
-^http[s]?:\/\/api.yonekura.cn\/3.6.8\/uicommon\/getuser.+$ requires-body=1,max-size=0,script-path=yonekura.js
+^http[s]?:\/\/api.yonekura.cn\/3.6.8\/uicommon\/getuser.+$ requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/yelinhuan001/gaoxiao/main/cd.js
 
 [MITM]
 hostname = *.yonekura.*
