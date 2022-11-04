17521ef06be0757280857dbeffd2651149e55e63
yelinhuan001 <61222411+yelinhuan001@users.noreply.github.com>
2022-11-04 11:01:52 +0800

    Create cu.js

---
+++ cu.js
@@ -0,0 +1,28 @@
+/*******************************
+  公众号:木木IOS分享
+关注了解更多新科技！！！
+cubox
+脚本名称:cubox
+使用声明：️此脚本仅供学习与交流，
+        请勿转载与贩卖！️️️
+群1077223830
+*******************************
+[rewrite_local]
+^http[s]?:\/\/cubox.pro\/c\/api\/userInfo.+$ url script-response-body cubox.js
+[mitm] 
+hostname = *.cubox.*
+*******************************
+Surge
+
+[Script]
+^http[s]?:\/\/cubox.pro\/c\/api\/userInfo.+$ requires-body=1,max-size=0,script-path=cubox.js
+
+[MITM]
+hostname = *.cubox.*
+
+*******************************/
+var obj = JSON.parse($response.body);
+    obj.admin= true,;
+obj.level= 1;
+obj.expireTime= 2099-12-12;
+    $done({body: JSON.stringify(obj)});
