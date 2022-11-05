2affe2baf0ca9800467abc0260e4572a7abc621a
yelinhuan001 <61222411+yelinhuan001@users.noreply.github.com>
2022-11-05 15:12:35 +0800

    Update yy.js

--- yy.js
+++ yy.js
@@ -1,27 +1,12 @@
-/*******************************
-  公众号:木木IOS分享
-关注了解更多新科技！！！
-一言
-脚本名称:一言
-使用声明：️此脚本仅供学习与交流，
-        请勿转载与贩卖！️️️
-群1077223830
-*******************************
 [rewrite_local]
-^http[s]?:\/\/app.yiyan.art.+$ url script-response-body yiyan.js
+
+# ～ 一言解锁会员权限（2022-07-21）@ddgksf2013
+^https:\/\/app\.yiyan\.art\/yiyan\/ url script-response-body https://gitlab.com/ddgksf2013/Cuttlefish/-/raw/master/Crack/yiyan.js
 
 [mitm] 
-hostname = *.yiyan.*
-*******************************
-Surge
 
-[Script]
-^http[s]?:\/\/app.yiyan.art.+$ requires-body=1,max-size=0,script-path=yiyan.js
+hostname=app.yiyan.art
 
-[MITM]
-hostname = *.yiyan.*
-
-*******************************/
-var body=$response.body;
-body = body.replace(/viptype\":"\d",'viptype":"4",');
-$done(body);
+***********************************/
+var body = $response.body.replace(/viptype":"1"/g, 'viptype":"4"')
+$done({ body });
