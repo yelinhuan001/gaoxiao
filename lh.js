28a0137b4fa9e07dee545c1390b24e0dd646de41
yelinhuan001 <61222411+yelinhuan001@users.noreply.github.com>
2022-12-19 08:03:38 +0800

    Update lh.js

--- lh.js
+++ lh.js
@@ -1,10 +1,25 @@
 /*******************************
-⚠️🛝此捷径仅适用于本地重写⚠️🛝
-  🧚🏻‍♂️作者：🍡魔法师🍡
-  🎈交流群：993539991
-🫧脚本名称:恋爱季节宝典搜索
-🫧建议配合working copy一起食用
-✈️下载地址https://apps.apple.com/app/id896694807✈️
+  公众号:木木IOS分享
+关注了解更多新科技！！！
+恋爱季节宝典搜索
+脚本名称:恋爱季节宝典搜索
+使用声明：️此脚本仅供学习与交流，
+        请勿转载与贩卖！️️️
+群1077223830
+*******************************
+[rewrite_local]
+^http[s]?:\/\/apic.qushuiyin.com\/index.php\/v4\/new\/member\/info.+$ url script-response-body qushuiyin.js
+[mitm] 
+hostname = *.qushuiyin.*
+*******************************
+Surge
+
+[Script]
+^http[s]?:\/\/apic.qushuiyin.com\/index.php\/v4\/new\/member\/info.+$ requires-body=1,max-size=0,script-path=qushuiyin.js
+
+[MITM]
+hostname = *.qushuiyin.*
+
 *******************************/
 var body=$response.body;
 body = body.replace(/"free_search\":\d/g,'"free_search":999');
