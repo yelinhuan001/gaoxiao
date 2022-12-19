d5bfa70079a676364a8781f8a313d938b1946044
yelinhuan001 <61222411+yelinhuan001@users.noreply.github.com>
2022-12-19 09:50:43 +0800

    Update lh.js

--- lh.js
+++ lh.js
@@ -7,12 +7,15 @@
         请勿转载与贩卖！️️️
 群1077223830
 *******************************
+
 [rewrite_local]
 ^http[s]?:\/\/apic.qushuiyin.com\/index.php\/v4\/new\/vip\/memberVip.+$ url script-response-body https://raw.githubusercontent.com/yelinhuan001/gaoxiao/main/lh.js
+
+
 [mitm] 
 hostname = *.apic.*
-*******************************/
 
+*******************************/
 var body=$response.body;
 body = body.replace(/"free_search\":\d/g,'"free_search":999');
 body = body.replace(/"search_sum\":\d,'"search_sum":999,');
