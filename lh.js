8c3af77be75f052de0857b4b006c8de48f8039b6
yelinhuan001 <61222411+yelinhuan001@users.noreply.github.com>
2022-12-20 08:39:40 +0800

    Update lh.js

--- lh.js
+++ lh.js
@@ -1,28 +1,28 @@
 /*******************************
   公众号:木木IOS分享
 关注了解更多新科技！！！
-电视家vip
-脚本名称:电视家vip
+清理大师VIP
+脚本名称:清理大师VIP
 使用声明：️此脚本仅供学习与交流，
         请勿转载与贩卖！️️️
 群1077223830
 *******************************
 [rewrite_local]
-^http[s]?:\/\/123.56.125.184 url script-response-body https://raw.githubusercontent.com/yelinhuan001/gaoxiao/main/lh.js
+^http[s]?:\/\/clean-xud.laoyinnianhua.com\/api\/guests\/login.+$ url script-response-body https://raw.githubusercontent.com/yelinhuan001/gaoxiao/main/lh.js
 [mitm] 
-hostname = 123.56.125.184
+hostname = *.laoyinnianhua.*
 *******************************
 Surge
 
 [Script]
-^http[s]?:\/\/123.56.125.184\/api\/v3\/user\/info.+$ requires-body=1,max-size=0,script-path=56.js
+^http[s]?:\/\/clean-xud.laoyinnianhua.com\/api\/guests\/login.+$ requires-body=1,max-size=0,script-path=laoyinnianhua.js
 
 [MITM]
-hostname = *.56.*
+hostname = *.laoyinnianhua.*
 
 *******************************/
+
 var body=$response.body;
-body = body.replace(/"equityTime\":\d,'"equityTime":4100434652000,');
-body = body.replace(/"adEquityTime\":\d,'"adEquityTime":4100434652000,');
-body = body.replace(/"isTopLevel\":\w,'"isTopLevel":true,');
+body = body.replace(/"vip\":\d,'"vip":1,');
+body = body.replace(/"vip_expire_date\":\d/g,'"vip_expire_date":4101410282000');
 $done(body);
