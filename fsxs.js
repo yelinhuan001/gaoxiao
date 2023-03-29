673c94cba6b9a8cb06ea4580285c08f0687a7215
yelinhuan001 <61222411+yelinhuan001@users.noreply.github.com>
2023-03-30 03:33:11 +0800

    Update lh.js

---
+++ fsxs.js
@@ -0,0 +1,17 @@
+/******************************
+番薯小说
+
+*******************************
+%¥
+[rewrite_local]
+^https?:\/\/g20\.manmeng168\.com\/v1\/client\/user\/completeUserInfo.*? url script-response-body fsxs.js
+
+
+[mitm]
+hostname = g20.manmeng168.com
+%¥
+*******************************/
+var body=$response.body;
+body = body.replace(/"vip\":\w+/g,'"vip":true');
+body = body.replace(/"vip_end_time\":\d+/g,'"vip_end_time":32493837668000');
+$done(body);
