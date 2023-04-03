c681958eb60042e1686bccc3d26398a60046f42f
yelinhuan001 <61222411+yelinhuan001@users.noreply.github.com>
2023-04-04 02:51:39 +0800

    Create wzbz.js

---
+++ wzbz.js
@@ -0,0 +1,18 @@
+/******************************
+
+问真八字
+*******************************
+%¥
+[rewrite_local]
+^https?:\/\/bzpp2\.iwzbz\.com.*? url script-response-body wzbz.js
+
+
+[mitm]
+hostname = bzpp2.iwzbz.com
+%¥
+*******************************/
+var body=$response.body;
+body = body.replace(/"expires\": ".*?\"/g,'"expires": "2999-01-01"');
+body = body.replace(/"vipLevel\": \d+/g,'"vipLevel": 3');
+body = body.replace(/"data\": ".*?\"/g,'"data": "100000,名留青史 飘然隐归"');
+$done(body);
