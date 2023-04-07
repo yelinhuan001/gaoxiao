ada4a297b7687abf9978b15149b3419bc45b4e3c
yelinhuan001 <61222411+yelinhuan001@users.noreply.github.com>
2023-04-08 00:06:31 +0800

    Update soul.js

--- soul.js
+++ soul.js
@@ -6,7 +6,7 @@ soul
 
 [rewrite_local]
 
-^https:\/\/api-pay\.soulapp\.cn|https:\/\/api-pay\.soulapp\.cn|https:\/\/api-chat\.soulapp\.cn\/chat\/limitInfo|https?:\/\/47\.98\.249\.183|https?:\/\/47\.98\.236\.150|https?:\/\/47\.98\.249\.183|https?:\/\/120\.55\.86\.207|https?:\/\/114\.215\.255\.94|https?:\/\/120\.55\.55\.115|https?:\/\/47\.98\.137\.249|https?:\/\/47\.98\.54\.106|https?:\/\/118\.31\.112\.221.*? url script-response-body ssss.js
+^https:\/\/api-pay\.soulapp\.cn|https:\/\/api-pay\.soulapp\.cn|https:\/\/api-chat\.soulapp\.cn\/chat\/limitInfo|https?:\/\/47\.98\.249\.183|https?:\/\/47\.98\.236\.150|https?:\/\/47\.98\.249\.183|https?:\/\/120\.55\.86\.207|https?:\/\/114\.215\.255\.94|https?:\/\/120\.55\.55\.115|https?:\/\/47\.98\.137\.249|https?:\/\/47\.98\.54\.106|https?:\/\/118\.31\.112\.221.*? url script-response-body https://raw.githubusercontent.com/yelinhuan001/gaoxiao/main/soul.js
 
 [mitm]
 
