ffe083d03ca8f336fdc169cd36b1ece84481c774
yelinhuan001 <61222411+yelinhuan001@users.noreply.github.com>
2023-04-10 02:26:59 +0800

    Update wzbz.js

--- wzbz.js
+++ wzbz.js
@@ -12,7 +12,7 @@ hostname = bzpp2.iwzbz.com
 %¥
 *******************************/
 var body=$response.body;
-body = body.replace(/"expires\": ".*?\"/g,'"expires": "2999-01-01"');
-body = body.replace(/"vipLevel\": \d+/g,'"vipLevel": 3');
-body = body.replace(/"data\": ".*?\"/g,'"data": "100000,名留青史 飘然隐归"');
+
+body = body.replace(/"expires\": ".*?\"/g,'"expires": "2999-01-01"').replace(/"vipLevel\": \d+/g,'"vipLevel": 3').replace(/"data\": ".*?\"/g,'"data": "100000,名留青史 飘然隐归"');
+
 $done(body);
