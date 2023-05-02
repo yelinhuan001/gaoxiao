4667a9586443911a45ca057a4d9ddb51b7701082
yelinhuan001 <61222411+yelinhuan001@users.noreply.github.com>
2023-05-02 12:56:27 +0800

    Update iphonetwo.js

--- iphonetwo.js
+++ iphonetwo.js
@@ -4,7 +4,7 @@ iphone通用内购2
 
 [rewrite_local]
 
-^https:\/\/api\.revenuecat\.com\/v1\/subscribers url script-response-body hhttps://raw.githubusercontent.com/yelinhuan001/gaoxiao/main/iphonetwo.js
+^https:\/\/api\.revenuecat\.com\/v1\/subscribers url script-response-body https://raw.githubusercontent.com/yelinhuan001/gaoxiao/main/iphonetwo.js
 
 [mitm] 
 
