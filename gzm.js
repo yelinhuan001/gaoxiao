71fd48f898fbf7f1ca61758639e04c3358e1e938
yelinhuan001 <61222411+yelinhuan001@users.noreply.github.com>
2023-05-03 20:31:30 +0800

    Update gzm.js

--- gzm.js
+++ gzm.js
@@ -3,7 +3,7 @@
 *******************************
 %¥
 [rewrite_local]
-^https?:\/\/singerdream\.com.*? url script-response-body gzm.js
+^https?:\/\/singerdream\.com.*? url script-response-body https://github.com/yelinhuan001/gaoxiao/blame/main/gzm.js
 
 [mitm]
 hostname = singerdream.com
